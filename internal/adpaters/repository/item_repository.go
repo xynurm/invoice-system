2b6fbc590aafe857844630ce1ba7e623a8e36d5a
xynurm <nyakridha24@gmail.com>
2023-12-11 00:30:44 +0700

    add endpoint getItem

--- internal/adpaters/repository/item_repository.go
+++ internal/adpaters/repository/item_repository.go
@@ -44,6 +44,38 @@ func (r *repository) FindItemsRepository(ctx context.Context) ([]models.Item, er
 	return items, err
 }
 
+func (r *repository) GetItemRepository(ctx context.Context, itemID int) (models.Item, error) {
+	var err error
+	var items models.Item
+
+	tx := r.db.WithContext(ctx).Begin()
+
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+			return
+		}
+	}()
+
+	if tx.Error != nil {
+		return items, tx.Error
+	}
+
+	err = tx.First(&items, "id=?", itemID).Error
+	if err != nil {
+		tx.Rollback()
+		return items, err
+	}
+
+	err = tx.Commit().Error
+	if err != nil {
+		tx.Rollback()
+		return items, err
+	}
+
+	return items, err
+}
+
 func (r *repository) CreateItemRepository(ctx context.Context, item models.Item) (models.Item, error) {
 	var err error
 
